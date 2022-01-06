5b9760c5fae144def7e83d20edca24763f921e3d
JDr-Etnetera <56111669+JDr-Etnetera@users.noreply.github.com>
2022-01-06 17:19:23 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-loops.everyInterval(500, function () {
+loops.everyInterval(1000, function () {
     serial.writeValue("C", input.compassHeading())
+    radio.sendValue("c", input.compassHeading())
 })
 basic.forever(function () {
 	
