740682a5cb2d0258f6a54b02e594e553964153a1
JDr-Etnetera <56111669+JDr-Etnetera@users.noreply.github.com>
2022-01-06 19:00:29 +0100

    Compas - v0.1

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
-loops.everyInterval(1000, function () {
-    serial.writeValue("C", input.compassHeading())
-    radio.sendValue("c", input.compassHeading())
+input.onButtonPressed(Button.A, function () {
+    RingbitCar.freestyle(-10, 10)
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    RingbitCar.freestyle(0, 0)
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "A") {
+        RingbitCar.freestyle(-10, 10)
+    } else if (name == "B") {
+        RingbitCar.brake()
+    }
+})
+let deltaC = 0
+radio.setGroup(61)
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+let lastCs = 0
+let CsCount = 0
+loops.everyInterval(1000, function () {
+    deltaC = lastCs / CsCount
+    serial.writeValue("D", deltaC)
+    radio.sendValue("D", deltaC)
+    CsCount = 0
+    lastCs = 0
+})
+loops.everyInterval(100, function () {
+    CsCount += 1
+    lastCs += input.compassHeading()
+    radio.sendValue("C", input.compassHeading())
 })
