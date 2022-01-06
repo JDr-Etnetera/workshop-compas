249293653e07783995f9162408055fdd0bae0c71
JDr-Etnetera <56111669+JDr-Etnetera@users.noreply.github.com>
2022-01-06 22:09:16 +0100

    initial

--- main.ts
+++ main.ts
@@ -1,30 +1,38 @@
+/**
+ * radio.onReceivedValue(function (name, value) {
+ * 
+ * if (name == "A") {
+ * 
+ * RingbitCar.freestyle(-10, 10)
+ * 
+ * } else if (name == "B") {
+ * 
+ * RingbitCar.brake()
+ * 
+ * }
+ * 
+ * })
+ */
 input.onButtonPressed(Button.A, function () {
     RingbitCar.freestyle(-10, 10)
 })
 input.onButtonPressed(Button.B, function () {
     RingbitCar.freestyle(0, 0)
 })
-radio.onReceivedValue(function (name, value) {
-    if (name == "A") {
-        RingbitCar.freestyle(-10, 10)
-    } else if (name == "B") {
-        RingbitCar.brake()
-    }
-})
-let deltaC = 0
-radio.setGroup(61)
-RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
-let lastCs = 0
 let CsCount = 0
+let lastCs = 0
+let deltaC = 0
+// radio.setGroup(61)
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 loops.everyInterval(1000, function () {
     deltaC = lastCs / CsCount
     serial.writeValue("D", deltaC)
-    radio.sendValue("D", deltaC)
+    // radio.sendValue("D", deltaC)
     CsCount = 0
     lastCs = 0
 })
+// radio.sendValue("C", input.compassHeading())
 loops.everyInterval(100, function () {
     CsCount += 1
     lastCs += input.compassHeading()
-    radio.sendValue("C", input.compassHeading())
 })
